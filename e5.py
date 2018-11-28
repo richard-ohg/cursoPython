b977837a7203bd0369ca0150c7da281690643916
richard-ohg <ricardoomar.angel@gmail.com>
2018-11-28 17:12:52 -0600

    Ejercicio 5 en clase

---
+++ e5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#UNAM-CERT
+
+eq1 = ['Juan Manuel','Ignacio','Valeria','Luis Antonio','Pedro Alejandro']
+eq2 = ['Diana Guadalupe','Jorge Luis','Jesika','Jesus Enrique','Rafael Alejandro']
+eq3 = ['Servando Miguel','Ricardo Omar','Laura Patricia','Isaias Abraham','Oscar']
+
+#expresion funcional:
+# 1) funcion lambda que sume las tres listas
+# 2) filtre la lista resultante para obtener a los que tienen un solo nombre (filter)
+# 3) convierta a mayusculas los nombres del resultado anterior (map)
+# 4) obtener una cadena con los nombres resultantes, separando los nombres con coma (reduce)
+#UNA SOLA EXPRESION
+
+#lambda lista1,lista2,lista3: lista1+lista2+lista3
+
+
+print( reduce(lambda x,y: x+","+y ,map(lambda lista: lista.upper(),filter(lambda lista: " " not in lista,(lambda lista1,lista2,lista3: lista1+lista2+lista3)(eq1,eq2,eq3)))) )
