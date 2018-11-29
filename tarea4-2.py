6aff6345afbc9e3eefc6a310d42e48f290dff42d
richard-ohg <ricardoomar.angel@gmail.com>
2018-11-29 12:23:11 -0600

    Funcion para sacar el nombre del dominio. Tarea 4 parte 2 terminada

--- tarea4-2.py
+++ tarea4-2.py
@@ -11,6 +11,8 @@ nombre_archivo="nmap.xml"
 def ips_host_prendidos_apagados():
 	'''
 		Función para hacer una lista de ip's de los host prendidos y los host apagados
+		Retorna:
+			lista con ip's de hostprendidos, lista con ip's de hostapagados
 	'''
 	lista_prendidos=['IPs de host prendidos']
 	lista_apagados=['IPs de host apagados']
@@ -28,7 +30,7 @@ def ips_host_puerto22_abiertos():
 	'''
 		Función para obtener las ip's de los host que tengan el puerto 22 abierto
 		Retorna:
-			lista con ip's4
+			lista con ip's
 	'''
 	p22=['IPs de host que tengan el puerto 22 abierto']
 	with open(nombre_archivo,'r') as file:
@@ -40,11 +42,12 @@ def ips_host_puerto22_abiertos():
 						p22.append(a.find('address').get('addr'))
 	return p22
 
+
 def ips_honeypot():
 	'''
 		Función para saber las ip's de los host que tienen servidor http honeypot
 		Retorna
-			cantidad de los diferentes servidores
+			lista de ip's 
 	'''
 	honeypot=['Host que son honeypot']
 	with open(nombre_archivo,'r') as file:
@@ -57,6 +60,22 @@ def ips_honeypot():
 	return honeypot
 
 
+def nombre_dominio():
+	'''
+		Función para saber los dominios junto a su IP
+		Retorna:
+			lista de tuplas que contiene ip y nombre de dominio 
+	'''
+ 	dominios=['IP,Nombres de dominio']
+	with open(nombre_archivo,'r') as file:
+		root = ET.fromstring(file.read())
+		for a in root.findall('host'):
+			for x in a.findall('hostnames'):
+				for y in x.findall('hostname'):
+					dominios.append((a.find('address').get('addr'),y.get('name')))
+	return dominios
+
+
 def crear_csv():
 	'''
 		Función para crear el archivo csv con las ip's que se piden
@@ -64,13 +83,16 @@ def crear_csv():
 	l_p,l_a = ips_host_prendidos_apagados()
 	p22 = ips_host_puerto22_abiertos()
 	honey = ips_honeypot()
+	dom = nombre_dominio()
 	with open('reporte.csv','w') as file:
 		employee_writer = csv.writer(file)
 		employee_writer.writerow(l_p)
 		employee_writer.writerow(l_a)
 		employee_writer.writerow(p22)
 		employee_writer.writerow(honey)
+		employee_writer.writerow(dom)
 
 
-# print host_prendidos_apagados()
-crear_csv()
+if __name__ == '__main__':
+	crear_csv()
+	
