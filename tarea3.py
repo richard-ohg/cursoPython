d06f99d7063ee0991fc57275f672d1a145ead821
richard-ohg <ricardoomar.angel@gmail.com>
2018-12-05 23:55:51 -0600

    Cambiar letras por numeros

--- tarea3.py
+++ tarea3.py
@@ -8,6 +8,7 @@
 import itertools as it
 import sys
 import optparse
+from random import choice
 
 def addOptions():
     parser = optparse.OptionParser()
@@ -28,8 +29,24 @@ def cambiar_min_mayus(lista):
 		lis=[palabra.replace(letra,letra.upper()) for letra in palabra]
 	return lis
 
-def cambiar():
-	pass
+def cambiar_letras_numeros(lista):
+	lis=[]
+	for palabra in lista:
+		if palabra in ['a','e','o']:
+			for letra in palabra:
+				if letra == 'a':
+					new_string = palabra.replace(letra,'4')
+					lis.append(new_string)
+				if letra == 'e':
+					new_string = palabra.replace(letra,'3')
+					lis.append(new_string)
+				if letra == 'o':
+					new_string = palabra.replace(letra,'0')
+					lis.append(new_string)
+		else:
+			lis.append(palabra)
+	return lis
+		
 
 def leer_palabras(archivo):
 	lista=[]
@@ -41,15 +58,24 @@ def leer_palabras(archivo):
 
 
 
-
 if __name__ == '__main__':
 	opts = addOptions()
 	if opts.file is None:
 		print "Debe contener un archivo con palabras"
 		sys.exit(1)
 	palabras = leer_palabras(opts.file)
+	print len(palabras)
 	with open('contrasenas.txt','w') as f:
-		for num in range(1,len(palabras)+1):
-			perm = permutaciones(palabras,num)
-			for pal in perm:
-				f.write(pal+'\n')
+		perm = permutaciones(palabras,2)
+		for pal in perm:
+			f.write(pal+'\n')
+		new_list=cambiar_min_mayus(perm)
+		for pal in new_list:
+			f.write(pal+'\n')
+		new_list=cambiar_letras_numeros(new_list)
+		for pal in new_list:
+			f.write(pal+'\n')
+		new_list=cambiar_letras_numeros(perm)
+		for pal in new_list:
+			f.write(pal+'\n')
+
